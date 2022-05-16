8d0f96fd62fae6485526f43e3459bb18248cf330
aidan.belton <aidan.belton@codeplay.com>
2022-05-16 14:54:36 +0100

    Update aspect name

--- SYCL/Basic/aspects.cpp
+++ SYCL/Basic/aspects.cpp
@@ -54,8 +54,8 @@ int main() {
     if (plt.has(aspect::fp64)) {
       std::cout << "  fp64" << std::endl;
     }
-    if (plt.has(aspect::bf16)) {
-      std::cout << " bf16" << std::endl;
+    if (plt.has(aspect::ext_oneapi_bfloat16)) {
+      std::cout << " ext_oneapi_bfloat16" << std::endl;
     }
     if (plt.has(aspect::int64_base_atomics)) {
       std::cout << "  base atomic operations" << std::endl;
